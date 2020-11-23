80355020af58e1205b7c9f48dea23edbaf5850d1
oylx <oylx2008@163.com>
2020-11-23 17:21:04 +0800

    完善路由和内容

--- src/router.ts
+++ src/router.ts
@@ -1,25 +1,28 @@
-import Home from "./views/home.vue";
-import Doc from "./views/doc.vue";
-import SwitchDemo from "./components/SwitchDemo.vue";
-import DialogDemo from "./components/DialogDemo.vue";
-import DocDemo from "./components/DocDemo.vue";
-import TabsDemo from "./components/TabsDemo.vue";
-import ButtonDemo from "./components/ButtonDemo.vue";
 import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import SwitchDemo from "./components/SwitchDemo.vue";
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
+import Intro from './views/Intro.vue';
+import GetStarted from './views/GetStarted.vue'
+import Install from './views/Install.vue'
 const history = createWebHashHistory();
 export const router = createRouter({
-  history,
+  history: history,
   routes: [
-    {
-      path: "/",
-      component: Home,
-    },
+    { path: "/", component: Home },
     {
       path: "/doc",
       component: Doc,
       children: [
         { path: "", component: DocDemo },
+        { path: "intro", component: Intro },
+        { path: "get-started", component: GetStarted },
+        { path: "install", component: Install },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
@@ -28,7 +31,6 @@ export const router = createRouter({
     },
   ],
 });
-
-router.afterEach((before, to) => {
-  console.log(before, to);
+router.afterEach(() => {
+  console.log("路由切换了");
 });
