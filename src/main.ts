e4ca1f25bf8d61de55f1348ced61e732f3f9fd5c
oylx <oylx2008@163.com>
2020-11-24 15:54:54 +0800

    引入GitHub样式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
 import "./lib/gulu.scss";
-import './index.scss'
-import { router } from './router'
+import "./index.scss";
+import { createApp } from "vue";
+import App from "./App.vue";
+import { router } from "./router";
 import './lib/svg.js'
+import 'github-markdown-css'
 
-const app = createApp(App)
-app.use(router)
-app.mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
--- src/router.ts
+++ src/router.ts
@@ -7,9 +7,9 @@ import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
 
-import Intro from './views/Intro.vue';
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
+import Intro from './markdown/Intro.vue';
+import GetStarted from './markdown/GetStarted.vue'
+import Install from './markdown/Install.vue'
 const history = createWebHashHistory();
 export const router = createRouter({
   history: history,
